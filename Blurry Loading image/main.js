afc4bbdc4e8892c0884533e84f14e5a351e47676
cobby <nkumsagoefredrick@gmail.com>
2021-08-17 22:14:37 -0300

    initial commit

---
+++ Blurry Loading image/main.js
@@ -0,0 +1,13 @@
+const loadText= docuent.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+let load= 0;
+let int= setInterval(blurring, 30)
+
+function blurring(){
+	load++
+	if(load>99){
+		clearInterval(int)
+	}
+	loadText.innerText= `${load}%`
+	
+}
