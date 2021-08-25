a7b03e936c9c78154fa22f17e0da62db3697b87e
cobby <nkumsagoefredrick@gmail.com>
2021-08-25 14:01:02 -0300

    dog app project

--- Blurry Loading image/main.js
+++ Blurry Loading image/main.js
@@ -1,4 +1,4 @@
-const loadText= docuent.querySelector('.loading-text')
+const loadText= document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 let load= 0;
 let int= setInterval(blurring, 30)
@@ -9,5 +9,10 @@ function blurring(){
 		clearInterval(int)
 	}
 	loadText.innerText= `${load}%`
-	
+	  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+// copied from stackoverflow 
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+ const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
