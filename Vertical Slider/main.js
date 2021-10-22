77db393b082be22d934958759c677f1f8579d972
cobby <nkumsagoefredrick@gmail.com>
2021-10-22 12:12:39 -0400

    random choice picker

--- Vertical Slider/main.js
+++ Vertical Slider/main.js
@@ -39,4 +39,3 @@ const changeSlide= (direction)=>{
 slideRight.style.transform= ` translateY(-${activeSlideIndex * sliderHeight}px )`
 slideLeft.style.transform= ` translateY(+${activeSlideIndex * sliderHeight}px )`
 }
-
