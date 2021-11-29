f3692b68118f49f770e11593b62de51796a41391
Cobby <nkumsagoefredrick@gmail.com>
2021-11-29 04:51:40 -0500

    fixed small bug

--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -4,10 +4,10 @@ const List = ({people}) => {
   return (
     <>
       {people.map((person )=>{
-        const {id,name, age,Image}=person;
+        const {id,name, age,image}=person;
         return (
         <article key={id} className="person">
-        <img src={Image} alt={name} />
+        <img src={image} alt={name} />
         <div>
           <h4>{name}</h4>
           <p>{age} years</p>
